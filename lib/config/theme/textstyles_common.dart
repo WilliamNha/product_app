120202f4b88293db8607221030a94673967643e2
WilliamNha <nhawilliam@gmail.com>
2025-04-26 11:49:13 +0700

    config text

---
+++ lib/config/theme/textstyles_common.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/material.dart';
+import 'package:product_app/gen/fonts.gen.dart';
+
+class TextstylesCommon {
+  static const text48bold = TextStyle(
+      fontSize: 48,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.normal);
+
+  static const text32Bold = TextStyle(
+      fontSize: 32,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.normal);
+
+  static const text30 = TextStyle(
+      fontSize: 30,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal);
+
+  static const text30Bold = TextStyle(
+      fontSize: 30,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.normal);
+
+  static const text26Bold = TextStyle(
+      fontSize: 26,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.normal);
+
+  static const text28Bold = TextStyle(
+      fontSize: 28,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.normal);
+
+  static const text26 = TextStyle(
+      fontSize: 26,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal);
+
+  static const text24Bold = TextStyle(
+      fontSize: 24,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.normal);
+
+  static const text24SemiBold = TextStyle(
+      fontSize: 24,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w600,
+      fontStyle: FontStyle.normal);
+
+  static const text24 = TextStyle(
+      fontSize: 24,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal);
+
+  static const text20Bold = TextStyle(
+      fontSize: 20,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.normal);
+
+  static const text20SemiBold = TextStyle(
+      fontSize: 20,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w600,
+      fontStyle: FontStyle.normal);
+
+  static const text20Medium = TextStyle(
+      fontSize: 20,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w500,
+      fontStyle: FontStyle.normal);
+
+  static const text20 = TextStyle(
+      fontSize: 20,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal);
+
+  static const text18Bold = TextStyle(
+      fontSize: 18,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.normal);
+
+  static const text18SemiBold = TextStyle(
+      fontSize: 18,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w600,
+      fontStyle: FontStyle.normal);
+
+  static const text18 = TextStyle(
+      fontSize: 18,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal);
+
+  static const text18Medium = TextStyle(
+      fontSize: 18,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w500,
+      fontStyle: FontStyle.normal);
+
+  static const text16Bold = TextStyle(
+      fontSize: 16,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.normal);
+
+  static const text16SemiBold = TextStyle(
+      fontSize: 16,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w600,
+      fontStyle: FontStyle.normal);
+
+  static const text16Medium = TextStyle(
+      fontSize: 16,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w500,
+      fontStyle: FontStyle.normal);
+
+  static const text16 = TextStyle(
+      fontSize: 16,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal);
+
+  static const text15Bold = TextStyle(
+      fontSize: 15,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.normal);
+
+  static const text15 = TextStyle(
+      fontSize: 15,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal);
+
+  static const text14Bold = TextStyle(
+      fontSize: 14,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.normal);
+
+  static const text14SemiBold = TextStyle(
+      fontSize: 14,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w600,
+      fontStyle: FontStyle.normal);
+
+  static const text14Medium = TextStyle(
+      fontSize: 14,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w500,
+      fontStyle: FontStyle.normal);
+
+  static const text14 = TextStyle(
+      fontSize: 14,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal);
+
+  static const text13 = TextStyle(
+      fontSize: 13,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal);
+
+  static const text13Bold = TextStyle(
+      fontSize: 13,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.normal);
+
+  static const text12Bold = TextStyle(
+      fontSize: 12,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.normal);
+
+  static const text12SemiBold = TextStyle(
+      fontSize: 12,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w600,
+      fontStyle: FontStyle.normal);
+
+  static const text12Medium = TextStyle(
+      fontSize: 12,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w500,
+      fontStyle: FontStyle.normal);
+
+  static const text12 = TextStyle(
+      fontSize: 12,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal);
+
+  static const text11 = TextStyle(
+      fontSize: 11,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal);
+
+  static const text10SemiBold = TextStyle(
+      fontSize: 10,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w600,
+      fontStyle: FontStyle.normal);
+
+  static const text10Medium = TextStyle(
+      fontSize: 10,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w500,
+      fontStyle: FontStyle.normal);
+
+  static const text10 = TextStyle(
+      fontSize: 10,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal);
+
+  static const text9Medium = TextStyle(
+      fontSize: 9,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w500,
+      fontStyle: FontStyle.normal);
+
+  static const text8 = TextStyle(
+      fontSize: 8,
+      fontFamily: FontFamily.inter,
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal);
+}
---
+++ lib/config/theme/theme_text.tailor.dart
@@ -0,0 +1,351 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, unused_element, unnecessary_cast
+
+part of 'theme_text.dart';
+
+// **************************************************************************
+// TailorAnnotationsGenerator
+// **************************************************************************
+
+mixin _$ThemeTextTailorMixin on ThemeExtension<ThemeText> {
+  TextStyle get text10;
+  TextStyle get text10Medium;
+  TextStyle get text10SemiBold;
+  TextStyle get text11;
+  TextStyle get text12;
+  TextStyle get text12Bold;
+  TextStyle get text12Medium;
+  TextStyle get text12SemiBold;
+  TextStyle get text13;
+  TextStyle get text13Bold;
+  TextStyle get text14;
+  TextStyle get text14Bold;
+  TextStyle get text14Medium;
+  TextStyle get text14SemiBold;
+  TextStyle get text15;
+  TextStyle get text15Bold;
+  TextStyle get text16;
+  TextStyle get text16Bold;
+  TextStyle get text16Medium;
+  TextStyle get text16SemiBold;
+  TextStyle get text18;
+  TextStyle get text18Bold;
+  TextStyle get text18Medium;
+  TextStyle get text18SemiBold;
+  TextStyle get text20;
+  TextStyle get text20Bold;
+  TextStyle get text20Medium;
+  TextStyle get text20SemiBold;
+  TextStyle get text24;
+  TextStyle get text24Bold;
+  TextStyle get text24SemiBold;
+  TextStyle get text26;
+  TextStyle get text26Bold;
+  TextStyle get text28Bold;
+  TextStyle get text30;
+  TextStyle get text30Bold;
+  TextStyle get text32Bold;
+  TextStyle get text48bold;
+  TextStyle get text8;
+  TextStyle get text9Medium;
+
+  @override
+  ThemeText copyWith({
+    TextStyle? text10,
+    TextStyle? text10Medium,
+    TextStyle? text10SemiBold,
+    TextStyle? text11,
+    TextStyle? text12,
+    TextStyle? text12Bold,
+    TextStyle? text12Medium,
+    TextStyle? text12SemiBold,
+    TextStyle? text13,
+    TextStyle? text13Bold,
+    TextStyle? text14,
+    TextStyle? text14Bold,
+    TextStyle? text14Medium,
+    TextStyle? text14SemiBold,
+    TextStyle? text15,
+    TextStyle? text15Bold,
+    TextStyle? text16,
+    TextStyle? text16Bold,
+    TextStyle? text16Medium,
+    TextStyle? text16SemiBold,
+    TextStyle? text18,
+    TextStyle? text18Bold,
+    TextStyle? text18Medium,
+    TextStyle? text18SemiBold,
+    TextStyle? text20,
+    TextStyle? text20Bold,
+    TextStyle? text20Medium,
+    TextStyle? text20SemiBold,
+    TextStyle? text24,
+    TextStyle? text24Bold,
+    TextStyle? text24SemiBold,
+    TextStyle? text26,
+    TextStyle? text26Bold,
+    TextStyle? text28Bold,
+    TextStyle? text30,
+    TextStyle? text30Bold,
+    TextStyle? text32Bold,
+    TextStyle? text48bold,
+    TextStyle? text8,
+    TextStyle? text9Medium,
+  }) {
+    return ThemeText(
+      text10: text10 ?? this.text10,
+      text10Medium: text10Medium ?? this.text10Medium,
+      text10SemiBold: text10SemiBold ?? this.text10SemiBold,
+      text11: text11 ?? this.text11,
+      text12: text12 ?? this.text12,
+      text12Bold: text12Bold ?? this.text12Bold,
+      text12Medium: text12Medium ?? this.text12Medium,
+      text12SemiBold: text12SemiBold ?? this.text12SemiBold,
+      text13: text13 ?? this.text13,
+      text13Bold: text13Bold ?? this.text13Bold,
+      text14: text14 ?? this.text14,
+      text14Bold: text14Bold ?? this.text14Bold,
+      text14Medium: text14Medium ?? this.text14Medium,
+      text14SemiBold: text14SemiBold ?? this.text14SemiBold,
+      text15: text15 ?? this.text15,
+      text15Bold: text15Bold ?? this.text15Bold,
+      text16: text16 ?? this.text16,
+      text16Bold: text16Bold ?? this.text16Bold,
+      text16Medium: text16Medium ?? this.text16Medium,
+      text16SemiBold: text16SemiBold ?? this.text16SemiBold,
+      text18: text18 ?? this.text18,
+      text18Bold: text18Bold ?? this.text18Bold,
+      text18Medium: text18Medium ?? this.text18Medium,
+      text18SemiBold: text18SemiBold ?? this.text18SemiBold,
+      text20: text20 ?? this.text20,
+      text20Bold: text20Bold ?? this.text20Bold,
+      text20Medium: text20Medium ?? this.text20Medium,
+      text20SemiBold: text20SemiBold ?? this.text20SemiBold,
+      text24: text24 ?? this.text24,
+      text24Bold: text24Bold ?? this.text24Bold,
+      text24SemiBold: text24SemiBold ?? this.text24SemiBold,
+      text26: text26 ?? this.text26,
+      text26Bold: text26Bold ?? this.text26Bold,
+      text28Bold: text28Bold ?? this.text28Bold,
+      text30: text30 ?? this.text30,
+      text30Bold: text30Bold ?? this.text30Bold,
+      text32Bold: text32Bold ?? this.text32Bold,
+      text48bold: text48bold ?? this.text48bold,
+      text8: text8 ?? this.text8,
+      text9Medium: text9Medium ?? this.text9Medium,
+    );
+  }
+
+  @override
+  ThemeText lerp(covariant ThemeExtension<ThemeText>? other, double t) {
+    if (other is! ThemeText) return this as ThemeText;
+    return ThemeText(
+      text10: TextStyle.lerp(text10, other.text10, t)!,
+      text10Medium: TextStyle.lerp(text10Medium, other.text10Medium, t)!,
+      text10SemiBold: TextStyle.lerp(text10SemiBold, other.text10SemiBold, t)!,
+      text11: TextStyle.lerp(text11, other.text11, t)!,
+      text12: TextStyle.lerp(text12, other.text12, t)!,
+      text12Bold: TextStyle.lerp(text12Bold, other.text12Bold, t)!,
+      text12Medium: TextStyle.lerp(text12Medium, other.text12Medium, t)!,
+      text12SemiBold: TextStyle.lerp(text12SemiBold, other.text12SemiBold, t)!,
+      text13: TextStyle.lerp(text13, other.text13, t)!,
+      text13Bold: TextStyle.lerp(text13Bold, other.text13Bold, t)!,
+      text14: TextStyle.lerp(text14, other.text14, t)!,
+      text14Bold: TextStyle.lerp(text14Bold, other.text14Bold, t)!,
+      text14Medium: TextStyle.lerp(text14Medium, other.text14Medium, t)!,
+      text14SemiBold: TextStyle.lerp(text14SemiBold, other.text14SemiBold, t)!,
+      text15: TextStyle.lerp(text15, other.text15, t)!,
+      text15Bold: TextStyle.lerp(text15Bold, other.text15Bold, t)!,
+      text16: TextStyle.lerp(text16, other.text16, t)!,
+      text16Bold: TextStyle.lerp(text16Bold, other.text16Bold, t)!,
+      text16Medium: TextStyle.lerp(text16Medium, other.text16Medium, t)!,
+      text16SemiBold: TextStyle.lerp(text16SemiBold, other.text16SemiBold, t)!,
+      text18: TextStyle.lerp(text18, other.text18, t)!,
+      text18Bold: TextStyle.lerp(text18Bold, other.text18Bold, t)!,
+      text18Medium: TextStyle.lerp(text18Medium, other.text18Medium, t)!,
+      text18SemiBold: TextStyle.lerp(text18SemiBold, other.text18SemiBold, t)!,
+      text20: TextStyle.lerp(text20, other.text20, t)!,
+      text20Bold: TextStyle.lerp(text20Bold, other.text20Bold, t)!,
+      text20Medium: TextStyle.lerp(text20Medium, other.text20Medium, t)!,
+      text20SemiBold: TextStyle.lerp(text20SemiBold, other.text20SemiBold, t)!,
+      text24: TextStyle.lerp(text24, other.text24, t)!,
+      text24Bold: TextStyle.lerp(text24Bold, other.text24Bold, t)!,
+      text24SemiBold: TextStyle.lerp(text24SemiBold, other.text24SemiBold, t)!,
+      text26: TextStyle.lerp(text26, other.text26, t)!,
+      text26Bold: TextStyle.lerp(text26Bold, other.text26Bold, t)!,
+      text28Bold: TextStyle.lerp(text28Bold, other.text28Bold, t)!,
+      text30: TextStyle.lerp(text30, other.text30, t)!,
+      text30Bold: TextStyle.lerp(text30Bold, other.text30Bold, t)!,
+      text32Bold: TextStyle.lerp(text32Bold, other.text32Bold, t)!,
+      text48bold: TextStyle.lerp(text48bold, other.text48bold, t)!,
+      text8: TextStyle.lerp(text8, other.text8, t)!,
+      text9Medium: TextStyle.lerp(text9Medium, other.text9Medium, t)!,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is ThemeText &&
+            const DeepCollectionEquality().equals(text10, other.text10) &&
+            const DeepCollectionEquality()
+                .equals(text10Medium, other.text10Medium) &&
+            const DeepCollectionEquality()
+                .equals(text10SemiBold, other.text10SemiBold) &&
+            const DeepCollectionEquality().equals(text11, other.text11) &&
+            const DeepCollectionEquality().equals(text12, other.text12) &&
+            const DeepCollectionEquality()
+                .equals(text12Bold, other.text12Bold) &&
+            const DeepCollectionEquality()
+                .equals(text12Medium, other.text12Medium) &&
+            const DeepCollectionEquality()
+                .equals(text12SemiBold, other.text12SemiBold) &&
+            const DeepCollectionEquality().equals(text13, other.text13) &&
+            const DeepCollectionEquality()
+                .equals(text13Bold, other.text13Bold) &&
+            const DeepCollectionEquality().equals(text14, other.text14) &&
+            const DeepCollectionEquality()
+                .equals(text14Bold, other.text14Bold) &&
+            const DeepCollectionEquality()
+                .equals(text14Medium, other.text14Medium) &&
+            const DeepCollectionEquality()
+                .equals(text14SemiBold, other.text14SemiBold) &&
+            const DeepCollectionEquality().equals(text15, other.text15) &&
+            const DeepCollectionEquality()
+                .equals(text15Bold, other.text15Bold) &&
+            const DeepCollectionEquality().equals(text16, other.text16) &&
+            const DeepCollectionEquality()
+                .equals(text16Bold, other.text16Bold) &&
+            const DeepCollectionEquality()
+                .equals(text16Medium, other.text16Medium) &&
+            const DeepCollectionEquality()
+                .equals(text16SemiBold, other.text16SemiBold) &&
+            const DeepCollectionEquality().equals(text18, other.text18) &&
+            const DeepCollectionEquality()
+                .equals(text18Bold, other.text18Bold) &&
+            const DeepCollectionEquality()
+                .equals(text18Medium, other.text18Medium) &&
+            const DeepCollectionEquality()
+                .equals(text18SemiBold, other.text18SemiBold) &&
+            const DeepCollectionEquality().equals(text20, other.text20) &&
+            const DeepCollectionEquality()
+                .equals(text20Bold, other.text20Bold) &&
+            const DeepCollectionEquality()
+                .equals(text20Medium, other.text20Medium) &&
+            const DeepCollectionEquality()
+                .equals(text20SemiBold, other.text20SemiBold) &&
+            const DeepCollectionEquality().equals(text24, other.text24) &&
+            const DeepCollectionEquality()
+                .equals(text24Bold, other.text24Bold) &&
+            const DeepCollectionEquality()
+                .equals(text24SemiBold, other.text24SemiBold) &&
+            const DeepCollectionEquality().equals(text26, other.text26) &&
+            const DeepCollectionEquality()
+                .equals(text26Bold, other.text26Bold) &&
+            const DeepCollectionEquality()
+                .equals(text28Bold, other.text28Bold) &&
+            const DeepCollectionEquality().equals(text30, other.text30) &&
+            const DeepCollectionEquality()
+                .equals(text30Bold, other.text30Bold) &&
+            const DeepCollectionEquality()
+                .equals(text32Bold, other.text32Bold) &&
+            const DeepCollectionEquality()
+                .equals(text48bold, other.text48bold) &&
+            const DeepCollectionEquality().equals(text8, other.text8) &&
+            const DeepCollectionEquality()
+                .equals(text9Medium, other.text9Medium));
+  }
+
+  @override
+  int get hashCode {
+    return Object.hashAll([
+      runtimeType.hashCode,
+      const DeepCollectionEquality().hash(text10),
+      const DeepCollectionEquality().hash(text10Medium),
+      const DeepCollectionEquality().hash(text10SemiBold),
+      const DeepCollectionEquality().hash(text11),
+      const DeepCollectionEquality().hash(text12),
+      const DeepCollectionEquality().hash(text12Bold),
+      const DeepCollectionEquality().hash(text12Medium),
+      const DeepCollectionEquality().hash(text12SemiBold),
+      const DeepCollectionEquality().hash(text13),
+      const DeepCollectionEquality().hash(text13Bold),
+      const DeepCollectionEquality().hash(text14),
+      const DeepCollectionEquality().hash(text14Bold),
+      const DeepCollectionEquality().hash(text14Medium),
+      const DeepCollectionEquality().hash(text14SemiBold),
+      const DeepCollectionEquality().hash(text15),
+      const DeepCollectionEquality().hash(text15Bold),
+      const DeepCollectionEquality().hash(text16),
+      const DeepCollectionEquality().hash(text16Bold),
+      const DeepCollectionEquality().hash(text16Medium),
+      const DeepCollectionEquality().hash(text16SemiBold),
+      const DeepCollectionEquality().hash(text18),
+      const DeepCollectionEquality().hash(text18Bold),
+      const DeepCollectionEquality().hash(text18Medium),
+      const DeepCollectionEquality().hash(text18SemiBold),
+      const DeepCollectionEquality().hash(text20),
+      const DeepCollectionEquality().hash(text20Bold),
+      const DeepCollectionEquality().hash(text20Medium),
+      const DeepCollectionEquality().hash(text20SemiBold),
+      const DeepCollectionEquality().hash(text24),
+      const DeepCollectionEquality().hash(text24Bold),
+      const DeepCollectionEquality().hash(text24SemiBold),
+      const DeepCollectionEquality().hash(text26),
+      const DeepCollectionEquality().hash(text26Bold),
+      const DeepCollectionEquality().hash(text28Bold),
+      const DeepCollectionEquality().hash(text30),
+      const DeepCollectionEquality().hash(text30Bold),
+      const DeepCollectionEquality().hash(text32Bold),
+      const DeepCollectionEquality().hash(text48bold),
+      const DeepCollectionEquality().hash(text8),
+      const DeepCollectionEquality().hash(text9Medium),
+    ]);
+  }
+}
+
+extension ThemeTextBuildContextProps on BuildContext {
+  ThemeText get themeText => Theme.of(this).extension<ThemeText>()!;
+  TextStyle get text10 => themeText.text10;
+  TextStyle get text10Medium => themeText.text10Medium;
+  TextStyle get text10SemiBold => themeText.text10SemiBold;
+  TextStyle get text11 => themeText.text11;
+  TextStyle get text12 => themeText.text12;
+  TextStyle get text12Bold => themeText.text12Bold;
+  TextStyle get text12Medium => themeText.text12Medium;
+  TextStyle get text12SemiBold => themeText.text12SemiBold;
+  TextStyle get text13 => themeText.text13;
+  TextStyle get text13Bold => themeText.text13Bold;
+  TextStyle get text14 => themeText.text14;
+  TextStyle get text14Bold => themeText.text14Bold;
+  TextStyle get text14Medium => themeText.text14Medium;
+  TextStyle get text14SemiBold => themeText.text14SemiBold;
+  TextStyle get text15 => themeText.text15;
+  TextStyle get text15Bold => themeText.text15Bold;
+  TextStyle get text16 => themeText.text16;
+  TextStyle get text16Bold => themeText.text16Bold;
+  TextStyle get text16Medium => themeText.text16Medium;
+  TextStyle get text16SemiBold => themeText.text16SemiBold;
+  TextStyle get text18 => themeText.text18;
+  TextStyle get text18Bold => themeText.text18Bold;
+  TextStyle get text18Medium => themeText.text18Medium;
+  TextStyle get text18SemiBold => themeText.text18SemiBold;
+  TextStyle get text20 => themeText.text20;
+  TextStyle get text20Bold => themeText.text20Bold;
+  TextStyle get text20Medium => themeText.text20Medium;
+  TextStyle get text20SemiBold => themeText.text20SemiBold;
+  TextStyle get text24 => themeText.text24;
+  TextStyle get text24Bold => themeText.text24Bold;
+  TextStyle get text24SemiBold => themeText.text24SemiBold;
+  TextStyle get text26 => themeText.text26;
+  TextStyle get text26Bold => themeText.text26Bold;
+  TextStyle get text28Bold => themeText.text28Bold;
+  TextStyle get text30 => themeText.text30;
+  TextStyle get text30Bold => themeText.text30Bold;
+  TextStyle get text32Bold => themeText.text32Bold;
+  TextStyle get text48bold => themeText.text48bold;
+  TextStyle get text8 => themeText.text8;
+  TextStyle get text9Medium => themeText.text9Medium;
+}
