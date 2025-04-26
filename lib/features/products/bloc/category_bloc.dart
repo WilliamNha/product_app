7d7752cc84e56530b69049289046ad9a0166f8a2
WilliamNha <nhawilliam@gmail.com>
2025-04-26 16:10:53 +0700

    category_bloc

---
+++ lib/features/products/bloc/category_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+import 'package:product_app/features/products/bloc/category_event.dart';
+import 'package:product_app/features/products/bloc/category_state.dart';
+import 'package:product_app/features/products/usecases/get_categories_usecase.dart';
+
+@injectable
+class CategoryBloc extends Bloc<CategoryEvent, CategoryState> {
+  final GetCategoriesUsecase _getCategoriesUsecase;
+
+  CategoryBloc(this._getCategoriesUsecase) : super(const CategoryLoading()) {
+    on<GetCategories>((event, emit) async {
+      emit(const CategoryLoading());
+      try {
+        final categories = await _getCategoriesUsecase.call();
+        emit(CategoriesDone(categories: categories));
+      } on DioException catch (dioError) {
+        emit(CategoryError(error: dioError.message ?? "Something went wrong"));
+      } catch (e) {
+        emit(CategoryError(error: e.toString()));
+      }
+    });
+  }
+}
