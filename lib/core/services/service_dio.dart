336e84e99b93eb8bfdfec647adce997435932c20
WilliamNha <nhawilliam@gmail.com>
2025-04-27 21:17:57 +0700

    add interceptor and timeout

--- lib/core/services/service_dio.dart
+++ lib/core/services/service_dio.dart
@@ -4,5 +4,10 @@ import 'package:injectable/injectable.dart';
 @module
 abstract class ServiceDio {
   @lazySingleton
-  Dio dio() => Dio();
+  Dio dio() {
+    final dio = Dio();
+    dio.options.connectTimeout = const Duration(seconds: 7);
+    dio.interceptors.add(LogInterceptor());
+    return dio;
+  }
 }
